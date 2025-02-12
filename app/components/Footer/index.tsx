937b66d667a3048b83cc1f737c34062bfd145fae
Caolan <caolan.fanning@gmail.com>
2025-02-11 19:08:28 -0500

    add transitions to footer and navbar

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,10 +1,7 @@
-import Image from 'next/image';
-import React from 'react';
-import linkedIn from '@/lib/assets/icons/linkedIn.svg';
-import github from '@/lib/assets/icons/github.svg';
-import discord from '@/lib/assets/icons/discord.svg';
+import { DiscordIcon } from '../Svgs/DiscordIcon';
+import { GithubIcon } from '../Svgs/GithubIcon';
+import { LinkedinIcon } from '../Svgs/LinkedInIcon';
 import './styles.css';
-import { GithubIcon } from '../GithubIcon';
 
 type Props = {};
 
@@ -13,32 +10,24 @@ export const Footer = (props: Props) => {
 		<footer>
 			<ul>
 				<li>
-					<a href='https://www.linkedin.com/in/caolan-fanning-1a7229157/'>
-						<Image
-							className='linkedIn'
-							src={linkedIn}
-							alt='LinkedIn Icon'
-							height={24}
-							width={24}
-						/>
+					<a
+						className='linkedin'
+						href='https://www.linkedin.com/in/caolan-fanning-1a7229157/'>
+						<LinkedinIcon />
 						<span>LinkedIn</span>
 					</a>
 				</li>
 				<li>
-					<a href='https://github.com/caolan93'>
+					<a className='github' href='https://github.com/caolan93'>
 						<GithubIcon />
 						<span>Github</span>
 					</a>
 				</li>
 				<li>
-					<a href='https://discord.com/channels/1216860101810454588/1216860101810454590'>
-						<Image
-							className='discord'
-							src={discord}
-							alt='Discord Icon'
-							height={24}
-							width={24}
-						/>
+					<a
+						className='discord'
+						href='https://discord.com/channels/1216860101810454588/1216860101810454590'>
+						<DiscordIcon />
 						<span>Discord</span>
 					</a>
 				</li>
--- app/components/Svgs/DiscordIcon.tsx
+++ app/components/Svgs/DiscordIcon.tsx
@@ -3,7 +3,7 @@ type IconProps = {
 	width?: string;
 };
 
-export const GithubIcon = ({
+export const DiscordIcon = ({
 	height = '1.5rem',
 	width = '1.5rem',
 }: IconProps) => {
