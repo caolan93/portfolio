36c5c40b3e9fca5c220d9438cc8b463aae65da10
Caolan <caolan.fanning@gmail.com>
2025-02-13 13:09:46 -0500

    set up email integration with sendgrid

---
+++ app/sections/Hero/Form/actions.ts
@@ -0,0 +1,41 @@
+'use server';
+import sendGrid from '@sendgrid/mail';
+
+// sgMail
+//   .send(msg)
+//   .then(() => {
+//     console.log('Email sent')
+//   })
+//   .catch((error) => {
+//     console.error(error)
+//   })
+
+export const sendEmail = async (formData: FormData) => {
+	sendGrid.setApiKey(process.env.SENDGRID_API_KEY ?? '');
+	const name = formData.get('name') ?? '';
+	const emailValue = formData.get('email') ?? '';
+	const messageValue = formData.get('message') ?? '';
+
+	if (typeof emailValue === 'string' && typeof messageValue === 'string') {
+		const email = emailValue;
+		const message = messageValue;
+		const msg = {
+			to: 'caolan.fanning@gmail.com',
+			from: 'caolan.fanning+sendgrid@gmail.com',
+			replyTo: email,
+			subject: `${name} wants to get in touch`,
+			text: message,
+		};
+
+		try {
+			if (!Object.values(msg).every((formData) => formData)) {
+				throw new Error('Form data cannot be empty of undefined');
+			}
+			await sendGrid.send(msg);
+		} catch (e: any) {
+			console.error(e.response);
+		}
+	} else {
+		console.error('Invalid email or message format.');
+	}
+};
--- app/sections/Hero/Form/index.tsx
+++ app/sections/Hero/Form/index.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import { useFormStatus } from 'react-dom';
 import './styles.css';
+import { sendEmail } from './actions';
 
 type Props = {};
+
+export function SubmitButton() {
+	const { pending } = useFormStatus();
+
+	return (
+		<button className='get-in-touch-button' disabled={pending} type='submit'>
+			Sign Up
+		</button>
+	);
+}
 
 const GetInTouch = (props: Props) => {
 	return (
 		<div className='form-container'>
 			<h2>Get in Touch</h2>
-			<form action=''>
+			<form action={sendEmail}>
 				<label htmlFor=''>
 					Name
 					<input name='name' type='text' required placeholder='Name...' />
 				</label>
 				<label htmlFor=''>
 					Email
-					<input name='name' type='email' required placeholder='Email...' />
+					<input name='email' type='email' required placeholder='Email...' />
 				</label>
 				<label htmlFor=''>
 					Message
@@ -25,7 +36,7 @@ const GetInTouch = (props: Props) => {
 						placeholder="What's going on..?"
 					/>
 				</label>
-				<button className='get-in-touch-button'>Send</button>
+				<SubmitButton />
 			</form>
 		</div>
 	);
