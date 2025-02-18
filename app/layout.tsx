b2116f87028722fb26706ee355f5834c15441c5e
Caolan <caolan.fanning@gmail.com>
2025-02-18 09:28:28 -0500

    remove react scan from prod

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
+import { Footer } from '@components/Footer';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { ReactScan } from './ReactScan';
-import { Footer } from '@components/Footer';
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -29,7 +28,6 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 				suppressHydrationWarning>
-				<ReactScan />
 				<main>{children}</main>
 				<Footer />
 			</body>
